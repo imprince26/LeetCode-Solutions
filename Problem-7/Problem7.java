5408dedc65787ecbb0b31de7838bb73d2791f513
imprince26 <princepatel260806@gmail.com>
2025-01-14 21:02:26 +0530

    update code

--- Problem-7/Problem7.java
+++ Problem-7/Problem7.java
@@ -5,8 +5,10 @@ public class Problem7 {
         while (x != 0) {
             int pop = x % 10;
             x /= 10;
-            if (reversed > Integer.MAX_VALUE/10 || (reversed == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
-            if (reversed < Integer.MIN_VALUE/10 || (reversed == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && pop > 7))
+                return 0;
+            if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && pop < -8))
+                return 0;
             reversed = reversed * 10 + pop;
         }
         return reversed;
