0f94c3750e899a0118b1f0e7a2f54f2771fe84d1
imprince26 <princepatel260806@gmail.com>
2025-01-14 21:01:07 +0530

    update code

--- Problem-7/Problem7.java
+++ Problem-7/Problem7.java
@@ -1,17 +1,22 @@
-/* 7.Reverse Integer */
-class Problem7 {
+// reverse integer
+public class Problem7 {
     public int reverse(int x) {
-        int res = 0;
-        boolean isNegative = x < 0;
-        String strX = String.valueOf(Math.abs(x));
-        StringBuilder sb = new StringBuilder(strX).reverse();
-        
-        try {
-            res = Integer.parseInt(sb.toString());
-        } catch (NumberFormatException e) {
-            return 0;
+        int reversed = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (reversed > Integer.MAX_VALUE/10 || (reversed == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (reversed < Integer.MIN_VALUE/10 || (reversed == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            reversed = reversed * 10 + pop;
         }
-        
-        return isNegative ? -res : res;  
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        Problem7 solution = new Problem7();
+        System.out.println(solution.reverse(123)); // Output: 321
+        System.out.println(solution.reverse(-123)); // Output: -321
+        System.out.println(solution.reverse(120)); // Output: 21
+        System.out.println(solution.reverse(0)); // Output: 0
     }
 }
