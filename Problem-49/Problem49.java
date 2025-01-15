e4edf40a95dab32ce51b0e8e97db0630a1738fa9
imprince26 <princepatel260806@gmail.com>
2025-01-15 13:17:18 +0530

    add code

---
+++ Problem-49/Problem49.java
@@ -0,0 +1,24 @@
+// Problem-49: Group Anagrams
+import java.util.*;
+
+public class Problem49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String keyStr = String.valueOf(ca);
+            if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<>());
+            map.get(keyStr).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        Problem49 solution = new Problem49();
+        String[] input = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> result = solution.groupAnagrams(input);
+        System.out.println(result);
+    }
+}
