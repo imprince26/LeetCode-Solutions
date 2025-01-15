19baa490364eb64b17d0dc39cd8678d6461fa2ad
imprince26 <princepatel260806@gmail.com>
2025-01-15 12:50:07 +0530

    add code

--- Problem-66/Problem66.java
+++ Problem-66/Problem66.java
@@ -1,5 +1,5 @@
 public class Problem66 {
-    public int[] plusOne(int[] digits) {
+    public int[] plusOne(int digits[]) {
         int n = digits.length;
         
         for (int i = n - 1; i >= 0; i--) {
@@ -10,16 +10,16 @@ public class Problem66 {
             digits[i] = 0;
         }
         
-        int[] newNumber = new int[n + 1];
+        int newNumber[] = new int[n + 1];
         newNumber[0] = 1;
         
         return newNumber;
     }
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         Problem66 solution = new Problem66();
-        int[] digits = {1, 2, 3};
-        int[] result = solution.plusOne(digits);
+        int digits[] = {1, 2, 3};
+        int result[] = solution.plusOne(digits);
         for (int digit : result) {
             System.out.print(digit + " ");
         }
