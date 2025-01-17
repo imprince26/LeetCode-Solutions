0925713cad67ac8f9f0de4279a723c5325c35d33
imprince26 <princepatel260806@gmail.com>
2025-01-17 17:36:42 +0530

    add code

---
+++ Problem-20/Problem20.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+public class Problem20 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Problem20 solution = new Problem20();
+        System.out.println(solution.isValid("()")); 
+        System.out.println(solution.isValid("()[]{}")); 
+        System.out.println(solution.isValid("(]")); 
+        System.out.println(solution.isValid("([)]")); 
+        System.out.println(solution.isValid("{[]}")); 
+    }
+}
