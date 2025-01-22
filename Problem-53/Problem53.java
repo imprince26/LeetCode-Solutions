60602df5b06d43201c97aa38813666da0495ff79
imprince26 <princepatel260806@gmail.com>
2025-01-22 07:58:38 +0530

    add Code

--- Problem-53/Problem53.java
+++ Problem-53/Problem53.java
@@ -2,18 +2,18 @@ public class Problem53 {
     public int maxSubArray(int[] nums) {
         int maxSoFar = nums[0];
         int maxEndingHere = nums[0];
-        
+
         for (int i = 1; i < nums.length; i++) {
             maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
             maxSoFar = Math.max(maxSoFar, maxEndingHere);
         }
-        
+
         return maxSoFar;
     }
 
     public static void main(String[] args) {
         Problem53 solution = new Problem53();
-        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
         System.out.println(solution.maxSubArray(nums)); // Output: 6
     }
 }
