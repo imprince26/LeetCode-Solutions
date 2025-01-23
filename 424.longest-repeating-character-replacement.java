579e4cc88a01f73fc3f7211af6d587bbd70d1183
imprince26 <princepatel260806@gmail.com>
2025-01-23 08:30:44 +0530

    add #424

---
+++ 424.longest-repeating-character-replacement.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=424 lang=java
+ *
+ * [424] Longest Repeating Character Replacement
+ */
+
+// @lc code=start
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] count = new int[26];
+        int maxCount = 0, maxLength = 0;
+        int start = 0;
+
+        for (int end = 0; end < s.length(); end++) {
+            maxCount = Math.max(maxCount, ++count[s.charAt(end) - 'A']);
+            while (end - start + 1 - maxCount > k) {
+                count[s.charAt(start) - 'A']--;
+                start++;
+            }
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+
+        return maxLength;
+    }
+}
+// @lc code=end
+
