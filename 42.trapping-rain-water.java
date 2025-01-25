6de23346be0e42f5a43752eb462a7a55a46fcc43
imprince26 <princepatel260806@gmail.com>
2025-01-25 08:01:09 +0530

    add code

---
+++ 42.trapping-rain-water.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=42 lang=java
+ *
+ * [42] Trapping Rain Water
+ */
+
+// @lc code=start
+class Solution {
+    public int trap(int[] height) {
+        int leftMax = 0, rightMax = 0;
+        int left = 0, right = height.length - 1;
+        int res = 0;
+        while (left <= right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    res += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    res += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
