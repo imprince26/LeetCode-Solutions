1b3dada3c342974f4e8b63cd2725c849ced74290
imprince26 <princepatel260806@gmail.com>
2025-01-25 09:04:58 +0530

    add code

--- 2.add-two-numbers.java
+++ 2.add-two-numbers.java
@@ -8,11 +8,11 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
@@ -43,4 +43,3 @@ class Solution {
     }
 }
 // @lc code=end
-
