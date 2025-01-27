d8801f2735dbb67480463ac2f456ddbd240c0e76
imprince26 <princepatel260806@gmail.com>
2025-01-27 15:44:30 +0530

    add code

---
+++ 54.spiral-matrix.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=54 lang=java
+ *
+ * [54] Spiral Matrix
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        List<Integer> res = new ArrayList<>();
+        int left = 0, right = n - 1, top = 0, bottom = m - 1;
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                res.add(matrix[i][right]);
+            }
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    res.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    res.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
