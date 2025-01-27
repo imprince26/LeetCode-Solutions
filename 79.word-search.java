9d1b48c12d22633c9f7d5060c1ca7dbf442dcc46
imprince26 <princepatel260806@gmail.com>
2025-01-27 17:44:30 +0530

    add code

--- 79.word-search.java
+++ 79.word-search.java
@@ -27,12 +27,16 @@ class Solution {
         if (k == word.length)
             return true;
         if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j]
-                || board[i][j] != word[k])
-            return false;
+                || board[i][j] != word[k]){
+
+                    return false;
+                }
         visited[i][j] = true;
         if (dfs(board, word, visited, i - 1, j, k + 1) || dfs(board, word, visited, i + 1, j, k + 1)
-                || dfs(board, word, visited, i, j - 1, k + 1) || dfs(board, word, visited, i, j + 1, k + 1))
-            return true;
+                || dfs(board, word, visited, i, j - 1, k + 1) || dfs(board, word, visited, i, j + 1, k + 1)){
+
+                    return true;
+                }
         visited[i][j] = false;
         return false;
     }
