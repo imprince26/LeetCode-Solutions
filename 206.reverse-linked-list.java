42109b16c076fa5549f84e38a838cccad7e659f6
imprince26 <princepatel260806@gmail.com>
2025-01-28 07:44:09 +0530

    add code

---
+++ 206.reverse-linked-list.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=206 lang=java
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        head = reverse(head, null);
+        return head;
+    }
+    public ListNode reverse(ListNode head, ListNode newHead) {
+        if (head == null) {
+            return newHead;
+        }
+        ListNode next = head.next;
+        head.next = newHead;
+        return reverse(next, head);
+    }
+}
+// @lc code=end
+
