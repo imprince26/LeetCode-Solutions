70a88b3501ebb126a6113245994f808dedc3b4c8
imprince26 <princepatel260806@gmail.com>
2025-01-29 20:51:39 +0530

    add code

---
+++ 47.permutations-ii.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=47 lang=java
+ *
+ * [47] Permutations II
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        Arrays.sort(nums);
+        backtrack(res, path, nums, used);
+        return res; 
+    }
+    void backtrack(List<List<Integer>> res, List<Integer> path, int[] nums, boolean[] used) {
+        if (path.size() == nums.length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                continue;
+            }
+            path.add(nums[i]);
+            used[i] = true;
+            backtrack(res, path, nums, used);
+            path.remove(path.size() - 1);
+            used[i] = false;
+        }
+    }
+}
+// @lc code=end
+
