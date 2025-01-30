2f80697b0d4d06dae82f0471783146fc2e964ea4
imprince26 <princepatel260806@gmail.com>
2025-01-30 08:40:36 +0530

    add code

--- 145.binary-tree-postorder-traversal.java
+++ 145.binary-tree-postorder-traversal.java
@@ -8,16 +8,16 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
@@ -40,4 +40,3 @@ class Solution {
 
 }
 // @lc code=end
-
