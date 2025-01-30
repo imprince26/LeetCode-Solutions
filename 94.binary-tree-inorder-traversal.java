d5c27c86d4224396b6ff6bb1d9f104ccf04e4cab
imprince26 <princepatel260806@gmail.com>
2025-01-30 08:27:51 +0530

    add code

---
+++ 94.binary-tree-inorder-traversal.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=94 lang=java
+ *
+ * [94] Binary Tree Inorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>(); 
+        Stack<TreeNode> stack = new Stack<>(); 
+        TreeNode curr = root; 
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr); 
+                curr = curr.left; 
+            }
+            curr = stack.pop(); 
+            result.add(curr.val); 
+            curr = curr.right; 
+        }
+        return result;
+        
+    }
+}
+// @lc code=end
+
