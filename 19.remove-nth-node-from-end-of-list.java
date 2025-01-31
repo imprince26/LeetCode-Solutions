8e0c6d9dfedc2a0ba2fda254929f447cf726a872
imprince26 <princepatel260806@gmail.com>
2025-01-31 19:02:54 +0530

    add code

--- 19.remove-nth-node-from-end-of-list.java
+++ 19.remove-nth-node-from-end-of-list.java
@@ -8,11 +8,11 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
@@ -33,4 +33,3 @@ class Solution {
     }
 }
 // @lc code=end
-
