5177ed2a38cba3a7fd4ebeb29ea8004145527a71
imprince26 <princepatel260806@gmail.com>
2025-02-03 09:16:33 +0530

    add code

---
+++ 70.climbing-stairs.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=70 lang=java
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) return 1;
+        int[] ans = new int[n + 1];
+        // base case
+        ans[1] = 1;
+        ans[2] = 2;
+        // dp
+        for (int i = 3; i <= n; i++) {
+            ans[i] = ans[i - 1] + ans[i - 2]; 
+        }
+        return ans[n];
+        
+    }
+}
+// @lc code=end
+
