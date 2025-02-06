35aec2d799c963c08ed9ea62a706b3b52dc86e40
imprince26 <princepatel260806@gmail.com>
2025-02-06 08:10:09 +0530

    add code

---
+++ 287.find-the-duplicate-number.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=287 lang=java
+ *
+ * [287] Find the Duplicate Number
+ */
+
+// @lc code=start
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+      while (true) {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        if (slow == fast) {
+          break;
+        }
+      }
+      slow = nums[0];
+      while (slow != fast) {
+        slow = nums[slow];
+        fast = nums[fast]; 
+      }
+      return slow;
+    }
+}
+// @lc code=end
+
