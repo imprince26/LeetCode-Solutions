e3fcc0737796746ebaaf4f1ecd27c39adcabf06e
imprince26 <princepatel260806@gmail.com>
2025-02-06 08:00:21 +0530

    add code

---
+++ 344.reverse-string.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=344 lang=java
+ *
+ * [344] Reverse String
+ */
+
+// @lc code=start
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while (left < right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+            left++;
+            right--;
+        }
+        
+    }
+}
+// @lc code=end
+
