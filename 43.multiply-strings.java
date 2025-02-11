7779eab4bf44ea2d322a13856e572a560923cfea
imprince26 <princepatel260806@gmail.com>
2025-02-11 20:51:21 +0530

    add code

--- 43.multiply-strings.java
+++ 43.multiply-strings.java
@@ -7,7 +7,8 @@
 // @lc code=start
 class Solution {
     public String multiply(String num1, String num2) {
-        if (num1.equals("0") || num2.equals("0")) return "0";
+        if (num1.equals("0") || num2.equals("0"))
+            return "0";
         int[] result = new int[num1.length() + num2.length()];
         for (int i = num1.length() - 1; i >= 0; i--) {
             for (int j = num2.length() - 1; j >= 0; j--) {
@@ -21,12 +22,12 @@ class Solution {
         }
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < result.length; i++) {
-            if (sb.length() == 0 && result[i] == 0) continue;
+            if (sb.length() == 0 && result[i] == 0)
+                continue;
             sb.append(result[i]);
         }
         return sb.toString();
-        
+
     }
 }
 // @lc code=end
-
