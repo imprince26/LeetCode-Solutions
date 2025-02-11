ed6e3da5050cac61ea35551af84a2ceece50df9c
imprince26 <princepatel260806@gmail.com>
2025-02-11 20:50:58 +0530

    add code

---
+++ 43.multiply-strings.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=43 lang=java
+ *
+ * [43] Multiply Strings
+ */
+
+// @lc code=start
+class Solution {
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) return "0";
+        int[] result = new int[num1.length() + num2.length()];
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                int a = num1.charAt(i) - '0';
+                int b = num2.charAt(j) - '0';
+                int product = a * b;
+                int sum = product + result[i + j + 1];
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < result.length; i++) {
+            if (sb.length() == 0 && result[i] == 0) continue;
+            sb.append(result[i]);
+        }
+        return sb.toString();
+        
+    }
+}
+// @lc code=end
+
