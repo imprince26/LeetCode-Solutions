20d68611cb47f98c249914ecf2b94c9b980b1096
imprince26 <princepatel260806@gmail.com>
2025-07-10 20:29:50 +0530

    feat: Add Leetcode #39, #409 & #455

---
+++ 455.assign-cookies.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=455 lang=java
+ *
+ * [455] Assign Cookies
+ */
+
+// @lc code=start
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int ans = 0;
+        int i = 0, j = 0;
+        while (i < g.length && j < s.length) {
+            if (g[i] <= s[j]) {
+                ans++;
+                i++;
+                j++;
+            } else
+                j++;
+        }
+        return ans;
+    }
+}
+// @lc code=end
